91a43e2d73e94e49ae7488104cfc01a29e3dc0c8
alienware <imperial.gauntlet@gmail.com>
2019-05-12 07:52:20 -0400

    lynx_update

--- code/ANN_lynx.R
+++ code/ANN_lynx.R
@@ -1,28 +1,31 @@
 library("forecast")
 library(tidyverse)
 
+# Read Data  and preprocess
 data1 = read.csv("/home/alienware/workdir/github/hybrid_forecasting/data/lynx.csv")
 data1
 data = data1
-
-
-normfunc = function(x){
-  
-  (x-mean(x,na.rm=TRUE))/(sd(x,na.rm=TRUE))
-}
 data$time=NULL
 data$X=NULL
 
+# Standardardizing Values Z-score
+normfunc = function(x){
+  (x-mean(x,na.rm=TRUE))/(sd(x,na.rm=TRUE))
+}
 data$value = apply(data[1],2,normfunc)
 data$value
+
+# Convert data to Time series
 data = ts(data, frequency=1, start=c(1821,1),)
 plot(data)
 
+# Fit the model using NNTAR and predict 14 steps
 NNTAR = nnetar(window(data,end=1920),size =5,p=7,decay=0.5, maxit=150,scale.inputs=TRUE)
 plot(forecast(NNTAR,h=14))
 predicted = forecast(NNTAR,h=14)
 predicted
 
+# Finde MSE
 pred= c()
 for ( i in predicted[[16]]){
   print((i))
@@ -31,7 +34,7 @@ for ( i in predicted[[16]]){
 test_set = c(window(data,start=1921))
 test_set
 pred
-length(pred)
-length(test_set)
-mean((pred-test_set)^2)
-data
+
+print(" The MSE is ")
+print(mean((pred-test_set)^2))
+
