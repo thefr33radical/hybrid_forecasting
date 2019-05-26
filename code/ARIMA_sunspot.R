b99337bc5e42acc19175dcd218d8fb9f6fc8b479
user <imperial.gauntlet@gmail.com>
2019-05-26 17:58:41 -0400

    sunspot updated

---
+++ code/ARIMA_sunspot.R
@@ -0,0 +1,38 @@
+library(forecast)
+library(ggplot2)
+library(forecast)
+library(tseries)
+
+
+library("forecast")
+library(tidyverse)
+
+# Read Data  and preprocess
+data1 = read.csv("/home/user/workdir/thefr33radical/hybrid_forecasting/data/sunspot.year.csv")
+data1
+data = data1
+data$time=NULL
+data$X=NULL
+
+# Standardardizing Values Z-score
+normfunc = function(x){
+  (x-mean(x,na.rm=TRUE))/(sd(x,na.rm=TRUE))
+}
+data$value = apply(data[1],2,normfunc)
+data$value
+
+data
+
+# Convert data to Time series
+data = ts(data, frequency=1, start=c(1700,1),)
+plot(data)
+
+fit<-auto.arima(data, seasonal=FALSE)
+# plot model, ACf and PACF plots
+tsdisplay(residuals(fit), lag.max=45, main='(1,1,1) Model Residuals')
+# print output of the model
+fit
+
+fcast <- forecast(fit, h=32)
+# Plot the forecast model
+plot(fcast)
